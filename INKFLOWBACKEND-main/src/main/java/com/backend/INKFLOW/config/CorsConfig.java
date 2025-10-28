b6148ff49cd4e486fe7534c1226c1f0600f0c6a3
Nathan Henrique <rm95268@estudante.fieb.edu.br>
2025-10-27 23:42:31 -0300

    Allow any origin in CORS

--- INKFLOWBACKEND-main/src/main/java/com/backend/INKFLOW/config/CorsConfig.java
+++ INKFLOWBACKEND-main/src/main/java/com/backend/INKFLOW/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://*stud-yteste*.vercel.app", "http://localhost:*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
